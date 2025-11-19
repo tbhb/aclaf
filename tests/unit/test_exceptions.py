133f049cde444806b0cd317f777823ed882b8b75
Tony Burns <tony@tonyburns.net>
2025-11-19 16:14:25 -0500

    test: fix exception test

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -4,4 +4,4 @@ from aclaf.exceptions import AclafError
 class TestAclafError:
     def test_default(self):
         error = AclafError()
-        assert str(error) == "An unexpected error occurred."
+        assert str(error) == ""
